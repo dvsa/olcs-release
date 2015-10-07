6a86f568d488fd92e9e0bdf608f3bb89c0029df7
Rob Caiger <rob@clocal.co.uk>
2015-10-07 13:23:59 +0100

    Version 0.1

--- versions.sh
+++ versions.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="0.1.1"
+OLCS_VERSION="0.1"
 
 # Define the individual application version
-BACKEND="0.2"
-INTERNAL="0.2"
-SELFSERVE="0.2"
-STATIC="0.2"
-EMAIL="0.2"
-SCANNING="0.2"
-POSTCODE="0.1"
+BACKEND="0.1"
+INTERNAL="0.1"
+SELFSERVE="0.1"
+STATIC="0.1"
+EMAIL="0.1"
+SCANNING="0.1"
