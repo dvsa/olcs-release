01e9cc7bcc7cb7941adf4a5047d4726fa0129faa
Rob Caiger <rob@clocal.co.uk>
2015-10-07 12:04:19 +0100

    Release script and first versions config

---
+++ versions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Define the version of the OLCS release
+OLCS_VERSION="0.1.1"
+
+# Define the individual application version
+BACKEND="0.2"
+INTERNAL="0.2"
+SELFSERVE="0.2"
+STATIC="0.2"
+EMAIL="0.2"
+SCANNING="0.2"
+POSTCODE="0.1"
