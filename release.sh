d7bba47f7b388b5e2bab38160334944c61b11328
Rob Caiger <rob@clocal.co.uk>
2015-11-05 15:57:45 +0000

    Updated for latest release

--- release.sh
+++ release.sh
@@ -10,5 +10,4 @@ olcs-backend/$BACKEND.tar.gz \
 olcs-internal/$INTERNAL.tar.gz \
 olcs-selfserve/$SELFSERVE.tar.gz \
 olcs-static/$STATIC.tar.gz \
-olcs-email/$EMAIL.tar.gz \
 olcs-scanning/$SCANNING.tar.gz
--- versions.sh
+++ versions.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="0.1.1"
+OLCS_VERSION="0.1.2"
 
 # Define the individual application version
-BACKEND="0.2"
-INTERNAL="0.2"
-SELFSERVE="0.2"
-STATIC="0.2"
-EMAIL="0.2"
+BACKEND="0.3"
+INTERNAL="0.3"
+SELFSERVE="0.3"
+STATIC="0.3"
 SCANNING="0.2"
