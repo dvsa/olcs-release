7a5837d31c9b770ea779eabc5628488418fa7758
Rob Caiger <rob@clocal.co.uk>
2015-11-13 17:13:13 +0000

    Updated release script

--- release.sh
+++ release.sh
@@ -3,18 +3,18 @@
 # Define all versions
 source ./versions.sh
 
-cp api.erb ../release/olcs-backend/api-$BACKEND.erb
-cp iuweb.erb ../release/olcs-internal/iuweb-$INTERNAL.erb
-cp ssweb.erb ../release/olcs-selfserve/ssweb-$SELFSERVE.erb
+cp api.erb ../release/api/$BACKEND.erb
+cp iuweb.erb ../release/iuweb/$INTERNAL.erb
+cp ssweb.erb ../release/ssweb/$SELFSERVE.erb
 
 cd ../release
 
 tar --transform='flags=r;s|\/|-|' -czf OLCS-$OLCS_VERSION.tar.gz \
 olcs-backend/$BACKEND.tar.gz \
-olcs-backend/api-$BACKEND.erb \
+api/$BACKEND.erb \
 olcs-internal/$INTERNAL.tar.gz \
-olcs-internal/iuweb-$INTERNAL.erb \
+iuweb/$INTERNAL.erb \
 olcs-selfserve/$SELFSERVE.tar.gz \
-olcs-selfserve/ssweb-$SELFSERVE.erb \
+ssweb/$SELFSERVE.erb \
 olcs-static/$STATIC.tar.gz \
 olcs-scanning/$SCANNING.tar.gz
