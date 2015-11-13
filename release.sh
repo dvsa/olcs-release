fd21aedc8d30c073028c07beb581c09e2b6dccf0
Rob Caiger <rob@clocal.co.uk>
2015-11-13 17:09:36 +0000

    Updated release script, updated versions for 1.1

--- release.sh
+++ release.sh
@@ -3,11 +3,18 @@
 # Define all versions
 source ./versions.sh
 
+cp api.erb ../release/olcs-backend/api-$BACKEND.erb
+cp iuweb.erb ../release/olcs-internal/iuweb-$INTERNAL.erb
+cp ssweb.erb ../release/olcs-selfserve/ssweb-$SELFSERVE.erb
+
 cd ../release
 
 tar --transform='flags=r;s|\/|-|' -czf OLCS-$OLCS_VERSION.tar.gz \
 olcs-backend/$BACKEND.tar.gz \
+olcs-backend/api-$BACKEND.erb \
 olcs-internal/$INTERNAL.tar.gz \
+olcs-internal/iuweb-$INTERNAL.erb \
 olcs-selfserve/$SELFSERVE.tar.gz \
+olcs-selfserve/ssweb-$SELFSERVE.erb \
 olcs-static/$STATIC.tar.gz \
 olcs-scanning/$SCANNING.tar.gz
--- versions.sh
+++ versions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="1.0"
+OLCS_VERSION="1.1"
 
 # Define the individual application version
-BACKEND="0.3"
-INTERNAL="0.3"
-SELFSERVE="0.3"
-STATIC="0.3"
+BACKEND="0.4"
+INTERNAL="0.4"
+SELFSERVE="0.4"
+STATIC="0.4"
 SCANNING="0.2"
