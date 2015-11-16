648886c5d2493efa3138534c77fe1dfc352fcc61
Rob Caiger <rob@clocal.co.uk>
2015-11-16 10:20:51 +0000

    Updated release

--- release.sh
+++ release.sh
@@ -7,6 +7,14 @@ cp api.erb ../release/api/$BACKEND.erb
 cp iuweb.erb ../release/iuweb/$INTERNAL.erb
 cp ssweb.erb ../release/ssweb/$SELFSERVE.erb
 
+if [ -d olcs-txt ]; then
+    git clone git@gitlab.inf.mgt.mtpdvsa:olcs/olcs-txc.git
+fi
+
+(cd olcs-txc && git checkout $TXCHANGE)
+
+cp olcs-txc/txc.war ../release/txc/$TXCHANGE.war
+
 cd ../release
 
 tar --transform='flags=r;s|\/|-|' -czf OLCS-$OLCS_VERSION.tar.gz \
@@ -17,4 +25,5 @@ iuweb/$INTERNAL.erb \
 olcs-selfserve/$SELFSERVE.tar.gz \
 ssweb/$SELFSERVE.erb \
 olcs-static/$STATIC.tar.gz \
-olcs-scanning/$SCANNING.tar.gz
+olcs-scanning/$SCANNING.tar.gz \
+txc/$TXCHANGE.war
