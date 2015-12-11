a8ea2625dacdb538927da63223ec86432434af66
Rob Caiger <rob@clocal.co.uk>
2015-12-11 12:46:17 +0000

    Updated release

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="1.1.1"
+OLCS_VERSION="1.2"
 
 # Define the individual application version
-BACKEND="0.5"
-INTERNAL="0.5"
-SELFSERVE="0.5"
-STATIC="0.5"
+BACKEND="0.6"
+INTERNAL="0.6"
+SELFSERVE="0.6"
+STATIC="0.6"
 SCANNING="0.2"
 TXCHANGE="0.1"
