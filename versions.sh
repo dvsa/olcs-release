5cfdf4251a9edac0cfbd1d66ef253632529d3d5c
Mat Evans <mat.evans@valtech.co.uk>
2016-01-22 15:48:30 +0000

    2.1.1 versions

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="2.0"
+OLCS_VERSION="2.1.1"
 
 # Define the individual application version
-BACKEND="2.0"
-INTERNAL="2.0"
-SELFSERVE="2.0"
-STATIC="2.0"
+BACKEND="2.1.1"
+INTERNAL="2.1.1"
+SELFSERVE="2.1.1"
+STATIC="2.1.1"
 SCANNING="0.2"
 TXCHANGE="0.1"
