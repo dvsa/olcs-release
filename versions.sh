8397eef219c8b5e2e6a299bf92d36c5dd7906d25
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-01-29 14:18:04 +0000

    update version numbers to 2.2

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="2.1.1"
+OLCS_VERSION="2.2"
 
 # Define the individual application version
-BACKEND="2.1.1"
-INTERNAL="2.1.1"
-SELFSERVE="2.1.1"
-STATIC="2.1.1"
+BACKEND="2.2"
+INTERNAL="2.2"
+SELFSERVE="2.2"
+STATIC="2.2"
 SCANNING="0.2"
 TXCHANGE="0.1"
