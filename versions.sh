2aff1e43e8afa135ad83ed798d43686424188d7a
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-02-03 14:04:03 +0000

    bump versions for 2.2.1

--- versions.sh
+++ versions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="2.2"
+OLCS_VERSION="2.2.1"
 
 # Define the individual application version
-BACKEND="2.2"
+BACKEND="2.2.1"
 INTERNAL="2.2"
 SELFSERVE="2.2"
 STATIC="2.2"
