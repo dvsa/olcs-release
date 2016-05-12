79010534c82295880dfa0dbbeb0cc473c29c3384
Mat Evans <mat.evans@valtech.co.uk>
2016-05-12 09:38:26 +0100

    sprint-128 release

--- versions.sh
+++ versions.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="print-spike-1.0.0"
+OLCS_VERSION="sprint-128"
 
 # Define the individual application version
-BACKEND="print-spike-1.0.0"
-INTERNAL="print-spike-1.0.0"
-SELFSERVE="print-spike-1.0.0"
-STATIC="print-spike-1.0.0"
-TEMPLATES="print-spike-1.0.0"
+BACKEND="sprint-128"
+INTERNAL="sprint-128"
+SELFSERVE="sprint-128"
+STATIC="sprint-128"
+TEMPLATES="sprint/128"
 ADDRESS_SERVICE="1.0"
 ADDRESS_ETL="1.0.1"
-ELASTIC="print-spike-1.0.0"
-ETL="print-spike-1.0.0"
+ELASTIC="sprint/128"
+ETL="sprint-128"
 TXCHANGE="0.1"
 OPENAM="1.1"
 OPENAM_CONFIG="3.3.1"
