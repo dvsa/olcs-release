520ff435aef5f62e8e9167b0274d87972f2b71cc
Mat Evans <mat.evans@valtech.co.uk>
2016-05-13 16:51:07 +0100

    Versions for 3.4

--- versions.sh
+++ versions.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="print-spike-2.0.0"
+OLCS_VERSION="3.4"
 
 # Define the individual application version
-BACKEND="print-spike-2.0.0"
-INTERNAL="print-spike-2.0.0"
-SELFSERVE="print-spike-2.0.0"
-STATIC="print-spike-1.0.0"
-TEMPLATES="print-spike-1.0.0"
+BACKEND="3.4"
+INTERNAL="3.4"
+SELFSERVE="3.4"
+STATIC="3.4"
+TEMPLATES="3.4"
 ADDRESS_SERVICE="1.0"
-ADDRESS_ETL="1.0.1"
-ELASTIC="print-spike-1.0.0"
-ETL="print-spike-1.0.0"
+ADDRESS_ETL="1.1"
+ELASTIC="3.4"
+ETL="3.4"
 TXCHANGE="0.1"
 OPENAM="1.1"
 OPENAM_CONFIG="3.3.1"
