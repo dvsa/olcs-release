d558bc16d0359a527650b6e7f8dd4012f4f149dc
Mat Evans <mat.evans@valtech.co.uk>
2016-05-13 13:04:32 +0100

    Versions for print-spike-2.0.0

--- versions.sh
+++ versions.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="sprint-128"
+OLCS_VERSION="print-spike-2.0.0"
 
 # Define the individual application version
-BACKEND="sprint-128"
-INTERNAL="sprint-128"
-SELFSERVE="sprint-128"
-STATIC="sprint-128"
-TEMPLATES="sprint/128"
+BACKEND="print-spike-2.0.0"
+INTERNAL="print-spike-2.0.0"
+SELFSERVE="print-spike-2.0.0"
+STATIC="print-spike-1.0.0"
+TEMPLATES="print-spike-1.0.0"
 ADDRESS_SERVICE="1.0"
 ADDRESS_ETL="1.0.1"
-ELASTIC="sprint/128"
-ETL="sprint-128"
+ELASTIC="print-spike-1.0.0"
+ETL="print-spike-1.0.0"
 TXCHANGE="0.1"
 OPENAM="1.1"
 OPENAM_CONFIG="3.3.1"
