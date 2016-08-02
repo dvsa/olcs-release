ca3c1fb368ca83150d022708da33ba8ed606ccc4
Ian Lindsay <ian@hemera-business-services.co.uk>
2016-08-02 11:55:45 +0100

    new version numbers for 3.6

--- versions.sh
+++ versions.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="3.5"
+OLCS_VERSION="3.6"
 
 # Define the individual application version
-BACKEND="3.5"
-INTERNAL="3.5"
-SELFSERVE="3.5"
-STATIC="3.5"
-TEMPLATES="3.5"
+BACKEND="3.6"
+INTERNAL="3.6"
+SELFSERVE="3.6"
+STATIC="3.6"
+TEMPLATES="3.6"
 ADDRESS_SERVICE="1.2"
 ADDRESS_ETL="1.2"
-ELASTIC="3.5"
-ETL="3.5"
+ELASTIC="3.6"
+ETL="3.6"
 TXCHANGE="0.1"
-OPENAM="1.1"
-OPENAM_CONFIG="3.5"
+OPENAM="1.2"
+OPENAM_CONFIG="3.6"
