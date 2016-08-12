373012d22c68d1c810515b11f978b4bacbe30146
Mat Evans <mat.evans@valtech.co.uk>
2016-08-12 14:45:30 +0100

    Versions for 3.7

--- versions.sh
+++ versions.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="3.6"
+OLCS_VERSION="3.7"
 
 # Define the individual application version
-BACKEND="3.6"
-INTERNAL="3.6"
-SELFSERVE="3.6"
-STATIC="3.6"
-TEMPLATES="3.6"
+BACKEND="3.7"
+INTERNAL="3.7"
+SELFSERVE="3.7"
+STATIC="3.7"
+TEMPLATES="3.7"
 ADDRESS_SERVICE="1.2"
 ADDRESS_ETL="1.2"
-ELASTIC="3.6"
-ETL="3.6"
+ELASTIC="3.7"
+ETL="3.7"
 TXCHANGE="0.1"
 OPENAM="1.2"
 OPENAM_CONFIG="3.6"
-GUIDES="1.0" # 5 Aug 2016 - Not Tested, check on next release
-REPORTS="NEED A VALUE HERE" # 5 Aug 2016 - Not Tested, check on next release
+GUIDES="1.0"
+REPORTS="3.7"
