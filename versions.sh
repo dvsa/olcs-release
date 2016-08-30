285680866c8c759191a0896c13cd5d6ed9414505
Mat Evans <mat.evans@valtech.co.uk>
2016-08-30 12:40:27 +0100

    Updated versions for 3.8

--- versions.sh
+++ versions.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Define the version of the OLCS release
-OLCS_VERSION="3.7"
+OLCS_VERSION="3.8"
 
 # Define the individual application version
-BACKEND="3.7"
-INTERNAL="3.7"
-SELFSERVE="3.7"
-STATIC="3.7"
+BACKEND="3.8"
+INTERNAL="3.8"
+SELFSERVE="3.8"
+STATIC="3.8"
 TEMPLATES="3.7"
 ADDRESS_SERVICE="1.2"
 ADDRESS_ETL="1.2"
 ELASTIC="3.7"
-ETL="3.7"
+ETL="3.8"
 TXCHANGE="0.1"
 OPENAM="1.2"
 OPENAM_CONFIG="3.6"
